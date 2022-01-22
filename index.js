0b4772667f653f0f3eadacf52f8e23db4485bedd
Ankit Srivastava <ankit@richpanel.com>
2022-01-23 05:06:48 +0530

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const router = require('express').Router()
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
